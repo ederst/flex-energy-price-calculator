bb73f538553160ce7224ecea24fa3233b973deaf
ederst <eder_stefan@gmx.at>
2026-03-25 18:25:09 +0100

    feat: add date range support for CLI

--- flex_energy_price_calculator/command/__init__.py
+++ flex_energy_price_calculator/command/__init__.py
@@ -8,43 +8,70 @@ from flex_energy_price_calculator.models.gogreenenergy.gogreenenergyflex import 
 )
 from flex_energy_price_calculator.models.noprovider.lastmonthavg import LastMonthAvg
 from flex_energy_price_calculator.models.oekostrom.oekoflow import OekoFlow10
+from dateutil.relativedelta import relativedelta
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: <YYYY-MM> <model>")
+    if len(sys.argv) != 4:
+        print("Usage: <YYYY-MM> <YYYY-MM> <model>")
         exit(1)
-    month = sys.argv[1]
-    model = sys.argv[2]
+    start_month = sys.argv[1]
+    end_month = sys.argv[2]
+    model_arg = sys.argv[3]
 
-    display_date = date.fromisoformat(f"{month}-01")
+    print(f"Calculating prices for {start_month} to {end_month}\n---")
 
     DEFAULT_CACHE_DIR.mkdir(exist_ok=True)
 
-    if model == "oekoflow1.0":
-        model = OekoFlow10(display_date)
-    elif model in ["gogreenenergyflex", "gogreenenergyflexplus"]:
-        model = GoGreenEnergyFlex(display_date)
-    elif model in ["gogreenenergyflexfuture", "gogreenenergyflexfutureplus"]:
-        model = GoGreenEnergyFlexFuture(display_date)
-    elif model in ["lmavg"]:
-        model = LastMonthAvg(display_date)
-    else:
-        print(f"Unknown model: {model}")
-        exit(1)
+    start_date = date.fromisoformat(f"{start_month}-01")
+    end_date = date.fromisoformat(f"{end_month}-01")
+    current_date = start_date
+    display_model = {
+        'prices': [],
+        'average_prices': [],
+        'status_messages': [],
+        'net_prices': [],
+        'gross_prices': [],
+    }
+    while current_date <= end_date:
+        if model_arg == "oekoflow1.0":
+            model = OekoFlow10(current_date)
+        elif model_arg in ["gogreenenergyflex", "gogreenenergyflexplus"]:
+            model = GoGreenEnergyFlex(current_date)
+        elif model_arg in ["gogreenenergyflexfuture", "gogreenenergyflexfutureplus"]:
+            model = GoGreenEnergyFlexFuture(current_date)
+        elif model_arg in ["lmavg"]:
+            model = LastMonthAvg(current_date)
+        else:
+            print(f"Unknown model: {model_arg}")
+            exit(1)
 
-    prices_string = "".join([f"\n  - {x[0]}: {x[1]}" for x in model.prices])
+        display_model['prices'].extend(model.prices)
+        display_model['average_prices'].append(model.average_price)
+        display_model['status_messages'].append(model.status_message),
+        display_model['net_prices'].append(model.net_price),
+        display_model['gross_prices'].append(model.gross_price),
+        current_date = current_date + relativedelta(months=1)
+
+
+    average_price = sum(display_model['average_prices']) / len(display_model['average_prices'])
+    net_price = sum(display_model['net_prices']) / len(display_model['net_prices'])
+    gross_price = sum(display_model['gross_prices']) / len(display_model['gross_prices'])
+
+    prices_string = "".join([f"\n  - {x[0]}: {x[1]}" for x in display_model['prices']])
 
     print(
         (
             f"stock prices: {prices_string}\n"
-            f"avg stock price: {model.average_price:5.2f}€/MWh\n"
+            f"avg stock price: {average_price:5.2f}€/MWh\n"
             "---\n"
-            f"msg: {model.status_message}\n"
-            f"net enduser price:   {model.net_price:5.2f}ct/KWh\n"
-            f"gross enduser price: {model.gross_price:5.2f}ct/KWh\n"
+            # f"msg: {model.status_message}\n"
+            f"net enduser price:   {net_price:5.2f}ct/KWh\n"
+            f"gross enduser price: {gross_price:5.2f}ct/KWh\n"
         )
     )
+
+
 
 
 if __name__ == "__main__":
